af24a7ea11417b48308df1a66d4891d526375551
Austin Macdonald <austin@redhat.com>
2017-09-10 13:34:39 -0400

    setup P50

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -14,3 +14,9 @@ alias dilp='doitlive play'
 alias reload!='. ~/.zshrc'
 alias vi="vim"
 alias v="vim"
+
+# Pulp
+alias pssh='cd ~/3dev/devel && vagrant ssh'
+alias jp='cd ~/3dev/pulp'
+alias jd='cd ~/3dev/devel'
+alias rmmig='rm -rf ~/3dev/pulp/platform/pulpcore/app/migrations'
