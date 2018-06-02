ebabeed73163703f96ff8826f032408b173b0087
Austin Macdonald <austin@redhat.com>
2018-06-02 16:14:55 -0400

    fix for p50

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -15,8 +15,21 @@ alias reload!='. ~/.zshrc'
 alias vi="vim"
 alias v="vim"
 
-# Pulp
+# Pulp 3
 alias pssh='cd ~/3dev/devel && vagrant ssh'
 alias jp='cd ~/3dev/pulp'
 alias jd='cd ~/3dev/devel'
-alias rmmig='rm -rf ~/3dev/pulp/platform/pulpcore/app/migrations'
+alias jpy='cd ~/3dev/pulp_python'
+alias jf='cd ~/3dev/pulp_file'
+
+# Pulp 2
+alias j2='cd ~/2dev/pulp'
+alias j2py='cd ~/3dev/pulp_python'
+alias j2d='cd ~/2dev/devel'
+alias rm-mig='rm -rf ~/3dev/pulp/platform/pulpcore/app/migrations'
+function httpu() {
+  http "$2" "http://pulp3.dev:8000/pulp/api/""$1"
+}
+
+# Warehouse
+alias jw-'cd ~/3dev/warehouse'
